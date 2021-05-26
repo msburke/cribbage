0b46233c44268b4f853dd97208d7a159f742be9a
Matt <burke.matt7@gmail.com>
2021-05-26 17:17:23 -0500

    Started adding point values to pegging. Check 15 and check 31 seem to work appropriately. Check pair is a bit bugged, but I'll make it work. Consider combining 4, 3, and 2 of a kind into one group. Would be a bit cleaner

--- cribbage_game.py
+++ cribbage_game.py
@@ -7,16 +7,159 @@ PEG_MAX = 31
 
 def main():
     deck = make_card_list()  # make a deck of cards
+    player_score = []  # empty list to serve as a place where we can store all points awarded to player
+    computer_score = []  # empty list to serve as a place where we can store all points awarded to the computer
     crib = []  # make an empty crib list
-    player_1 = 'Matt'  # input('What is your name? ')  # asks for a player's name to use during the game.
+    player = 'Matt'  # input('What is your name? ')  # asks for a player's name to use during the game.
     computer = 'Computer'  # sets the computer's name to Computer
     player_hand = draw_hand(deck)  # draws cards for the player's hand
     computer_hand = draw_hand(deck)  # draws cards for the computer's hand
-    crib_holder = determine_first_crib(player_1, computer)  # randomly determine first crib
+    show_scores(player, player_score, computer_score)
+    crib_holder = determine_first_crib(player, computer)  # randomly determine first crib
     build_crib(player_hand, computer_hand, crib)  # clean up the crib building function. Both "players" in one
     community = draw_card(deck)  # draw the community card
-    show_community_card(community)
-    pegging(player_1, player_hand, computer, computer_hand, crib_holder)  # function for the pegging portion of scoring
+    show_community_card(community)  # shows the community card
+    pegging(player, player_hand, player_score, computer, computer_hand, computer_score, crib_holder)  # pegging
+    player_hand.append(community)  # adds the community card to the player's hand for scoring purposes
+    computer_hand.append(community)  # adds the community card to the computer's hand for scoring purposes
+    crib.append(community)  # adds the community card to the crib for scoring purposes
+
+
+def add_scores(points, player, player_score, computer_score, turn):
+    """
+    add the awarded points to the appropriate player
+    :param points: number of points to be awarded
+    :param player: name of the player
+    :param player_score: player's score list
+    :param computer_score: computer's score list
+    :param turn: whose turn is it?
+    :return: no return, values are appended to score lists
+    """
+    if turn == player:  # if it is the player's turn
+        player_score.append(points)  # give the points to the player
+    else:  # if it's the computer's turn
+        computer_score.append(points)  # give the points to the computer
+
+
+def check_peg_points(peg_list, peg_score, player, player_score, computer_score, turn):
+    """
+    checks all the ways to score points while pegging
+    :param peg_list: list of cards that have been played in the pegging round
+    :param peg_score: current peg score
+    :param player: name of player
+    :param player_score: player's score list
+    :param computer_score: computer's score list
+    :param turn: whose turn is it?
+    :return: no return, points will be appended
+    """
+    check_fifteen(peg_score, player, player_score, computer_score, turn)
+    check_thirty_one(peg_score, player, player_score, computer_score, turn)
+    check_pair(peg_list, player, player_score, computer_score, turn)
+    # check_three_of_kind()
+    # check_four_of_kind()
+    # check_run()
+
+
+def check_pair(peg_list, player, player_score, computer_score, turn):
+    if len(peg_list) >= 2:
+        last_play = peg_list[-1]
+        previous_play = peg_list[-2]
+        with open('deck_of_cards.csv') as f:  # opens the csv file
+            deck = csv.DictReader(f)  # reads the csv file as dictionary
+            for cards in deck:  # TODO: clean this up by getting the value of each card separately (make a new function) then compare them at the end
+                name = cards['Name']
+                order = cards['Order']
+                if name == last_play:
+                    last_play = order
+                if name == previous_play:
+                    previous_play = order
+                if last_play == previous_play:
+                    print(f'{turn} scores 2 points for a pair.')
+                    if turn == player:
+                        player_score.append(2)
+                    else:
+                        computer_score.append(2)
+
+def check_fifteen(peg_score, player, player_score, computer_score, turn):
+    """
+    Checks to see if the player/computer got 15 points on pegging
+    :param peg_score: current peg score
+    :param player: name of player
+    :param player_score: list of player's points
+    :param computer_score: list of computer's points
+    :param turn: whose turn is it?
+    :return: no return, values are appended
+    """
+    if peg_score == 15:  # checks to see if peg score is 15
+        print(f'{turn} scores 2 points for 15.')  # announces the player who got 15
+        if turn == player:  # if it was the player's turn
+            player_score.append(2)  # give the player 2 points
+        else:  # if it was the computer's turn
+            computer_score.append(2)  # give the computer 2 points
+
+
+def check_thirty_one(peg_score, player, player_score, computer_score, turn):
+    """
+    Checks to see if the player/computer got 31 points on pegging
+    :param peg_score: current peg score
+    :param player: name of player
+    :param player_score: list of player's points
+    :param computer_score: list of computer's points
+    :param turn: whose turn is it?
+    :return: no return, values are appended
+    """
+    if peg_score == 31:  # checks to see if peg score is 31
+        print(f'{turn} scores 2 points for 31.')  # announces player who got 31
+        if turn == player:  # if it was the player's turn
+            player_score.append(2)  # award player 2 points
+        else:  # if it was the computer's turn
+            computer_score.append(2)  # give the computer two points
+
+
+def show_scores(player, player_list, computer_list):
+    """
+    Hack way of keeping track of scores since lists are mutable. The lists keep track of all points awarded, then this
+    function will tabulate them for display
+    :param player: name of the player
+    :param player_list: list of all points awarded to the player
+    :param computer_list: list of all points awarded to the computer
+    :return: no return, but the function will display scores and quit the game when someone wins
+    """
+    player_score = int(score_from_list(player_list))  # call function to get score for player
+    computer_score = int(score_from_list(computer_list))  # call function to get score for computer
+    print(f"{player}'s score: {player_score}.")  # display the player's name and score
+    print(f"Computer's score: {computer_score}.")  # display the computer's score
+    check_for_win(player, player_score, computer_score)  # check of there's a winner
+
+
+def check_for_win(player, player_score, computer_score):
+    """
+    Compares player and computer scores to the value needed to win (Default: 121). Ends game if there's a winner
+    :param player: name of the player
+    :param player_score: player's score (int)
+    :param computer_score: computer's score (int)
+    :return: return None of there are no winners. Ends game if there is.
+    """
+    if player_score >= WINNING:  # compare player's score to winning score
+        print(f'Game over! {player} wins!')  # if the player has won, declare winner!
+        quit()  # quit the game
+    elif computer_score >= WINNING:  # compare computer's score to winning score
+        print('Game over, Computer wins!')  # if the player has won, declare winner!
+        quit()  # quit the game
+    else:  # if nobody has won
+        return None  # return nothing
+
+
+def score_from_list(lst):
+    """
+    Takes in the list values and returns an integer score
+    :param lst: List of points awarded to player/computer
+    :return: calculated score
+    """
+    score = 0  # start with a score of zero
+    for points in lst:  # go through every item in the list
+        score += points  # add the item to the score
+    return score  # return the score
 
 
 def show_community_card(community):
@@ -38,26 +181,30 @@ def determine_first_crib(player, computer):  # randomly determines who gets the 
         return computer  # computer gets the crib
 
 
-def pegging(player, player_hand, computer, computer_hand, crib_holder):  # pegging function
+def pegging(player, player_hand, player_score, computer, computer_hand, computer_score, crib_holder):
     """
+    This is the pegging function. Players will alternate playing cards up to 31 points until both players run out
+    of cards. Point values shown in score functions.
     :param player: Player's name
+    :param player_score: list of points awarded to the player
     :param player_hand: List containing the cards in the player's hand
     :param computer: 'Computer'
     :param computer_hand: List containing the cards in the computer's hand
+    :param computer_score: list of points awarded to the computer
     :param crib_holder: Who has the crib (player or computer)
     :return: none
-
     TODO: This needs some serious cleaning. Woof
     """
-    peg_count = 0  # start with pegging score of 0
     player_peg = player_hand.copy()  # make a copy of the player's hand so we can remove things from it
     computer_peg = computer_hand.copy()  # make copy of the computer's hand
     turn = get_next_turn(player, False, computer, False, crib_holder)  # player w/o crib goes first
     while len(player_peg) > 0 or len(computer_peg) > 0:  # as long as there are cards in someone's hand
+        peg_count = 0  # start with pegging score of 0
+        peg_list = []  # list of cards that have been played. Used for determining pairs, runs, etc.
         player_go = False  # flag for whether the player has said 'go'
         computer_go = False  # flag for whether the computer has said 'go'
         while not player_go or not computer_go:  # continue as long as at least one player hasn't said 'go'
-            peg_return = play_card(player, player_peg, computer, computer_peg, turn, peg_count)  # return int or 'go'
+            peg_return = play_card(player, player_peg, computer, computer_peg, turn, peg_list, peg_count)  # int or 'go'
             if type(peg_return) == str:  # checks for 'go' returned by player
                 print(f'{turn} says "Go"')  # announces 'go'
                 if turn == player:  # if the player returns go
@@ -66,16 +213,19 @@ def pegging(player, player_hand, computer, computer_hand, crib_holder):  # peggi
                     computer_go = True  # mark go flag for computer
             else:
                 peg_count += peg_return  # if a card was returned, adds value to peg count
+                check_peg_points(peg_list, peg_count, player, player_score, computer_score, turn)
             if peg_count == PEG_MAX:  # if the pegging count reaches 31
-                print(f'{turn} gets 31 for two points.')  # award two points to the player who hit 31
                 peg_count = 0  # reset the pegging count
+                peg_list = []  # clears the peg list for the next round
+                player_go = False  # reset player go to False
+                computer_go = False  # reset computer go to False
             show_peg_count(peg_count)
+            show_scores(player, player_score, computer_score)
             turn = get_next_turn(player, player_go, computer, computer_go, turn)  # switch players
             if len(player_peg) == 0:  # if the player is out of cards
                 player_go = True  # automatically say 'go'
             if len(computer_peg) == 0:  # if the computer is out of cards
                 computer_go = True  # automatically say 'go'
-        peg_count = 0  # reset pegging count if both players say go
 
 
 def show_peg_count(peg_count):
@@ -84,7 +234,7 @@ def show_peg_count(peg_count):
     print('--------------------------')
 
 
-def play_card(player, player_hand, computer, computer_hand, turn, peg):  # playing cards in pegging sequence
+def play_card(player, player_hand, computer, computer_hand, turn, peg_list, peg):  # playing cards in pegging sequence
     """
     This is the sequence for playing a card. Different sections for Player vs. AI
     :param player: Name of player
@@ -92,6 +242,7 @@ def play_card(player, player_hand, computer, computer_hand, turn, peg):  # playi
     :param computer: 'Computer'
     :param computer_hand: list of cards in computer's hand
     :param turn: whose turn is it?
+    :param peg_list: list of cards played in the current round of pegging.
     :param peg: current pegging score
     :return: value of card played, or string 'go'
 
@@ -104,6 +255,7 @@ def play_card(player, player_hand, computer, computer_hand, turn, peg):  # playi
         play = input(f'{player}, choose a card to play: ')  # ask for card to play
         if is_int(play):  # checks if the player input a value that can be an integer
             play_name = player_hand.pop(int(play) - 1)  # remove card from hand, store name
+            peg_list.append(play_name)  # add the card to the pegging list
             value = get_card_value(player, play_name)  # function for getting the value of a card
             return int(value)  # return the integer value of the card
         else:  # if the player inputs a string
@@ -120,6 +272,7 @@ def play_card(player, player_hand, computer, computer_hand, turn, peg):  # playi
                         if int(value) + peg <= PEG_MAX:  # if the value of the card can be added to peg count
                             play_name = computer_hand.pop(computer_hand.index(card))  # remove card from hand
                             value = get_card_value(computer, play_name)  # play the card
+                            peg_list.append(play_name)  # adds the card to the list of cards played
                             return int(value)  # return the value
             return 'Go'  # if no cards can be played, say 'go'
 

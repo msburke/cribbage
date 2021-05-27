d9103eaed7167a08b131fc4d87ea60356f7f12f9
Matt <burke.matt7@gmail.com>
2021-05-27 12:02:16 -0500

    Finished up pegging, finally! My groups (pair, 3, 4) could use some cleaning up, but at least it is functional right now. Next up, scoring the hands!

--- cribbage_game.py
+++ cribbage_game.py
@@ -6,23 +6,39 @@ PEG_MAX = 31
 
 
 def main():
-    deck = make_card_list()  # make a deck of cards
     player_score = []  # empty list to serve as a place where we can store all points awarded to player
     computer_score = []  # empty list to serve as a place where we can store all points awarded to the computer
     crib = []  # make an empty crib list
     player = 'Matt'  # input('What is your name? ')  # asks for a player's name to use during the game.
     computer = 'Computer'  # sets the computer's name to Computer
-    player_hand = draw_hand(deck)  # draws cards for the player's hand
-    computer_hand = draw_hand(deck)  # draws cards for the computer's hand
-    show_scores(player, player_score, computer_score)
     crib_holder = determine_first_crib(player, computer)  # randomly determine first crib
-    build_crib(player_hand, computer_hand, crib)  # clean up the crib building function. Both "players" in one
-    community = draw_card(deck)  # draw the community card
-    show_community_card(community)  # shows the community card
-    pegging(player, player_hand, player_score, computer, computer_hand, computer_score, crib_holder)  # pegging
-    player_hand.append(community)  # adds the community card to the player's hand for scoring purposes
-    computer_hand.append(community)  # adds the community card to the computer's hand for scoring purposes
-    crib.append(community)  # adds the community card to the crib for scoring purposes
+    while True:
+        deck = make_card_list()  # make a deck of cards
+        player_hand = draw_hand(deck)  # draws cards for the player's hand
+        computer_hand = draw_hand(deck)  # draws cards for the computer's hand
+        show_scores(player, player_score, computer_score)
+        build_crib(player_hand, computer_hand, crib, crib_holder)  # discard from hand to crib
+        community = draw_card(deck)  # draw the community card
+        show_community_card(community)  # shows the community card
+        pegging(player, player_hand, player_score, computer, computer_hand, computer_score, crib_holder)  # pegging
+        count_hands(player, player_score, player_hand, computer, computer_score, computer_hand, community, crib_holder, crib)
+        crib_holder = get_next_turn(player, False, computer, False, crib_holder)  # swap crib holder for the next turn
+
+
+def count_hands(player, player_score, player_hand, computer, computer_score, computer_hand, community, crib_holder, crib):
+    """
+    Count all of the possible points in the player and computer hands
+    :param computer: 'Computer'
+    :param player: name of the player
+    :param player_score: list of player points
+    :param player_hand: list of cards in the player's hand
+    :param computer_score: list of computer points
+    :param computer_hand: list of cards in the computer's hand
+    :param community: name of the community card
+    :param crib_holder: name of the who has the crib this round
+    :param crib: list of cards in the crib
+    :return:
+    """
 
 
 def add_scores(points, player, player_score, computer_score, turn):
@@ -54,31 +70,158 @@ def check_peg_points(peg_list, peg_score, player, player_score, computer_score, 
     """
     check_fifteen(peg_score, player, player_score, computer_score, turn)
     check_thirty_one(peg_score, player, player_score, computer_score, turn)
-    check_pair(peg_list, player, player_score, computer_score, turn)
-    # check_three_of_kind()
-    # check_four_of_kind()
-    # check_run()
+    check_groups(peg_list, player, player_score, computer_score, turn)
+    check_run(peg_list, player, player_score, computer_score, turn)
 
 
-def check_pair(peg_list, player, player_score, computer_score, turn):
-    if len(peg_list) >= 2:
-        last_play = peg_list[-1]
-        previous_play = peg_list[-2]
-        with open('deck_of_cards.csv') as f:  # opens the csv file
-            deck = csv.DictReader(f)  # reads the csv file as dictionary
-            for cards in deck:  # TODO: clean this up by getting the value of each card separately (make a new function) then compare them at the end
-                name = cards['Name']
-                order = cards['Order']
-                if name == last_play:
-                    last_play = order
-                if name == previous_play:
-                    previous_play = order
-                if last_play == previous_play:
-                    print(f'{turn} scores 2 points for a pair.')
-                    if turn == player:
-                        player_score.append(2)
-                    else:
-                        computer_score.append(2)
+def check_run(peg_list, player, player_score, computer_score, turn):
+    """
+    Check for possible runs
+    :param peg_list: list of cards played
+    :param player: name of player
+    :param player_score: list of player points
+    :param computer_score: list of computer points
+    :param turn: whose turn is it?
+    :return: points are announced and appended
+    """
+    if len(peg_list) >= 3:  # as long as the list is three or longer
+        for i in range(len(peg_list), 2, -1):  # try runs in decreasing value
+            if run_of_n(peg_list, player, player_score, computer_score, turn, i):  # if a run is successful..
+                break  # break out of the loop
+
+
+def run_of_n(peg_list, player, player_score, computer_score, turn, n):
+    """
+
+    :param peg_list: list of cards that have been played
+    :param player: name of player
+    :param player_score: list of player points
+    :param computer_score: list of computer points
+    :param turn: whose turn is it?
+    :param n: length of run to test for
+    :return: True if points are awarded, then points are announced and appended. False if not
+    """
+    n = int(n)
+    last_n = []  # list of the last n cards played
+    card_order = []  # order number of the last three cards played
+    for i in range(-1, -(n + 1), -1):  # get the last n cards played
+        last_n.append(peg_list[i])  # put them in the list above
+    for cards in last_n:  # look at the cards in the last n list
+        order = int(get_card_order(cards))  # get the order of the cards
+        card_order.append(order)  # put them in the list
+    if check_consecutive(card_order):  # check to see if the cards are consecutive, award appropriate number of points
+        if turn == player:
+            print(f'{player} played a run of {n} for {n} points.')
+            player_score.append(n)
+            return True
+        else:
+            print(f'Computer played a run of {n} for {n} points.')
+            computer_score.append(n)
+            return True
+    else:
+        return False
+
+
+def check_consecutive(cards):
+    """
+    Function to see if the cards played are consecutive in order to determine runs
+    :param cards: list of the order of the cards, should be a list of integers
+    :return: True if the cards can be arranged consecutively, False if they can't
+    """
+    sorted_list = sorted(cards)
+    range_list = list(range(min(cards), max(cards)+1))
+    if sorted_list == range_list:
+        return True
+    else:
+        return False
+
+
+def check_groups(peg_list, player, player_score, computer_score, turn):
+    """
+    Checks to see if there are there are possible pairs, three of a kind, or four of a kind
+    :param peg_list: list of cards that have been played during pegging round
+    :param player: name of player
+    :param player_score: list of player's points
+    :param computer_score: list of computer's points
+    :param turn: whose turn is it?
+    :return: points are appended during play
+    """
+    if len(peg_list) >= 4:  # if there are 4 or more cards in the hand, check for four of a kind first
+        four_of_a_kind(peg_list, player, player_score, computer_score, turn)
+    elif len(peg_list) == 3:  # if there are three cards in the hand, check for three of a kind first
+        three_of_a_kind(peg_list, player, player_score, computer_score, turn)
+    elif len(peg_list) == 2:  # if there are two cards in the hand, check for a pair first
+        two_of_a_kind(peg_list, player, player_score, computer_score, turn)
+
+
+def two_of_a_kind(peg_list, player, player_score, computer_score, turn):
+    """
+    Checks if the two most recently played cards are the same number or face
+    :param peg_list: list of cards played during pegging round
+    :param player: name of player
+    :param player_score: list of player points
+    :param computer_score: list of computer points
+    :param turn: whose turn is it
+    :return: points are appended during play
+    """
+    second = get_card_order(peg_list[-1])  # gets the "order" of the last card played. Same cards have same order
+    first = get_card_order(peg_list[-2])  # gets "order" of the second to last card played
+    if second == first:  # if the two cards are the same order, player/computer gets two points
+        print(f'{turn} played a pair for 2 points.')
+        if turn == player:
+            player_score.append(2)
+        else:
+            computer_score.append(2)
+
+
+def three_of_a_kind(peg_list, player, player_score, computer_score, turn):
+    """
+    Checks if the three most recently played cards are a three of a kind. If not, it'll try for a pair
+    :param peg_list: list of previously played cards
+    :param player: name of player
+    :param player_score: list of player points
+    :param computer_score: list of computer points
+    :param turn: whose turn is it?
+    :return: points will be displayed and appended
+    """
+    third = get_card_order(peg_list[-1])
+    second = get_card_order(peg_list[-2])  # these three variables get the 'order' of three most recently played cards
+    first = get_card_order(peg_list[-3])
+    if third == second == first:  # if all three cards are the same, award 6 points
+        print(f'{turn} played three of a kind for 6 points.')
+        if turn == player:
+            player_score.append(6)
+        else:
+            computer_score.append(6)
+    elif third == second:  # if only the most recently played are the same, go to the two of a kind (pair) function
+        two_of_a_kind(peg_list, player, player_score, computer_score, turn)
+
+
+def four_of_a_kind(peg_list, player, player_score, computer_score, turn):
+    """
+    Test for four of a kind. If not applicable, tries for three, then pair
+    :param peg_list: list of cards played
+    :param player: name of player
+    :param player_score: list of player points
+    :param computer_score: list of computer points
+    :param turn: whose turn is it
+    :return: points will be displayed and appended
+    """
+    fourth = get_card_order(peg_list[-1])
+    third = get_card_order(peg_list[-2])  # these functions will get the order
+    second = get_card_order(peg_list[-3])  # of the four most recent cards
+    first = get_card_order(peg_list[-4])
+    if fourth == third == second == first:  # if all 4 are the same, reward 12 points
+        print(f'{turn} played four of a kind for 12 points.')
+        if turn == player:
+            player_score.append(12)
+        else:
+            computer_score.append(12)
+    elif fourth == third == second:  # if only last three are the same, go to three of a kind function
+        three_of_a_kind(peg_list, player, player_score, computer_score, turn)
+    elif fourth == third:  # if only last two are the same, go to pair function
+        two_of_a_kind(peg_list, player, player_score, computer_score, turn)
+
 
 def check_fifteen(peg_score, player, player_score, computer_score, turn):
     """
@@ -221,11 +364,26 @@ def pegging(player, player_hand, player_score, computer, computer_hand, computer
                 computer_go = False  # reset computer go to False
             show_peg_count(peg_count)
             show_scores(player, player_score, computer_score)
+            show_pile(peg_list)
             turn = get_next_turn(player, player_go, computer, computer_go, turn)  # switch players
             if len(player_peg) == 0:  # if the player is out of cards
                 player_go = True  # automatically say 'go'
             if len(computer_peg) == 0:  # if the computer is out of cards
                 computer_go = True  # automatically say 'go'
+
+
+def show_pile(pile):
+    """
+    Used to show the cards that have been played in pegging so the player doesn't have to look back
+    :param pile: Takes in the cards from the pile
+    :return: no return, but will print on screen
+    """
+    print()  # print a blank line to keep things clean
+    print('Cards in play:', end=' ')  # prints the line 'Cards in play:' and takes out the line break
+    for cards in pile:
+        print(cards, end='. ')  # prints all the cards on the same line separated by periods
+    print()  # return at the end of the list
+    print()  # extra line to keep it clean
 
 
 def show_peg_count(peg_count):
@@ -303,8 +461,38 @@ def get_card_value(player, card):
             name = cards['Name']  # assign name to the key for each item in the 'Name' column
             value = cards['Value']  # assign value to the key for each item in the 'Value' column
             if name == card:  # looks up the name of the card drawn, returns the associated value
-                print(f'{player} played {name} for {value} points')  # displays the player, card, and value
+                print(f'{player} played {name} for {value}')  # displays the player, card, and value
                 return value  # returns value of card
+
+
+def get_card_order(card):
+    """
+    Looks up the name of the card on the csv and returns the order of the card
+    :param card: name of car
+    :return: order of the card
+    """
+    with open('deck_of_cards.csv') as f:
+        deck = csv.DictReader(f)
+        for cards in deck:
+            name = cards['Name']
+            order = cards['Order']
+            if name == card:
+                return order
+
+
+def get_card_suit(card):
+    """
+    Looks up the name of the card and returns its suit
+    :param card: name of the card
+    :return: name of the suit
+    """
+    with open('deck_of_cards.csv') as f:
+        deck = csv.DictReader(f)
+        for cards in deck:
+            name = cards['Name']
+            suit = cards['Suit']
+            if name == card:
+                return suit
 
 
 def get_next_turn(player, p_go, computer, c_go, turn):
@@ -332,21 +520,23 @@ def get_next_turn(player, p_go, computer, c_go, turn):
         return player  # make it the player's turn
 
 
-def build_crib(player_hand, computer_hand, crib):
+def build_crib(player_hand, computer_hand, crib, crib_holder):
     """
     Function for discarding cards to the crib
+    :param crib_holder: name of player who gets the crib
     :param player_hand: list of cards in the player's hand
     :param computer_hand: list of cards in the computer's hand
     :param crib: empty crib list
     :return: crib list appended in function
     """
-    discard_to_crib(player_hand, crib, False)  # ask player to discard two cards to the crib
-    discard_to_crib(computer_hand, crib, True)  # discards two random cards for the computer
+    discard_to_crib(player_hand, crib, False, crib_holder)  # ask player to discard two cards to the crib
+    discard_to_crib(computer_hand, crib, True, crib_holder)  # discards two random cards for the computer
 
 
-def discard_to_crib(hand, crib, ai):
+def discard_to_crib(hand, crib, ai, crib_holder):
     """
     takes in hand and crib for player/ai to discard 2 cards from hand to crib
+    :param crib_holder: name of player who gets the crib
     :param hand: list of cards in hand of player or computer
     :param crib: crib list (starts empty)
     :param ai: Is the player a player or computer? False = Player, True = AI
@@ -356,7 +546,9 @@ def discard_to_crib(hand, crib, ai):
         for i in range(2):  # player will discard two cards
             for num, card in enumerate(hand, start=1):  # numbers all of the cards
                 print(f'{num}. {card}')  # displays the numbers and the corresponding cards
-            discard = int(input('Select a card to place in the crib: '))  # prompts for discard
+            discard = int(input(f"Select a card to place in {crib_holder}'s crib: "))  # prompts for discard
+            while discard > len(hand) or discard < 1:
+                discard = int(input('Choose a valid card: '))
             crib.append(hand.pop(discard-1))  # removes the card from the player's hand and puts it in the crib
     else:  # for if player is computer (AI)
         for i in range(2):  # to discard two cards
